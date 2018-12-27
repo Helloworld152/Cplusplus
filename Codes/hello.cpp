ca07e738fc1d9341dd9a1669805a9636f4acb749
ruanying <15224718167@163.com>
2018-12-27 12:39:18 +0800

    update

--- Codes/hello.cpp
+++ Codes/hello.cpp
@@ -1,1 +1,1 @@
-//// Created by ruanying on 18-12-26.//#include <iostream>using namespace std;int main(){    cout << "hello";}
+//// Created by ruanying on 18-12-26.//#include <iostream>#include <bitset>using namespace std;void translate(int i){    if (i == 0)        return;    else{        translate(i / 10);        cout << i%10;        return;    }}int main(){    translate(4038);}
