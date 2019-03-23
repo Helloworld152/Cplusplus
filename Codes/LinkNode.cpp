7af442c08a56a8a6353f03569b563b13f63e6b58
阮颖 <15224718167@163.com>
2019-03-23 17:59:12 +0800

    update

--- Codes/LinkNode.cpp
+++ Codes/LinkNode.cpp
@@ -1,1 +1,32 @@
-//// Created by 阮颖 on 2019/3/11.//#include "LinkNode.h"template <typename elemType>sLinkNode<elemType>::Node * sLinkNode<elemType>::move(int i) const {    Node * p = head;    while (i-- >= 0) p = p->next;    return p;}template <class elemType>sLinkNode<elemType>::sLinkNode() {    head = new Node;    currentLength = 0;}template <class elemType>void sLinkNode<elemType>::clear() {    Node * p = head->next, *q;    head->next = NULL;    while (p != NULL){        q = p->next;    }}
+//
+// Created by 阮颖 on 2019/3/11.
+//
+
+#include "LinkNode.h"
+
+
+template <typename elemType>
+sLinkNode<elemType>::Node * sLinkNode<elemType>::move(int i) const {
+
+    Node * p = head;
+    while (i-- >= 0) p = p->next;
+    return p;
+}
+
+template <class elemType>
+sLinkNode<elemType>::sLinkNode() {
+
+    head = new Node;
+    currentLength = 0;
+}
+
+template <class elemType>
+void sLinkNode<elemType>::clear() {
+
+    Node * p = head->next, *q;
+    head->next = NULL;
+    while (p != NULL){
+        q = p->next;
+
+    }
+}
