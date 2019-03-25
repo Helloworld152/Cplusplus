85fe4a6a53313670de7f9f13f2771761adfa7b44
阮颖 <15224718167@163.com>
2019-03-25 20:51:22 +0800

    update

--- Codes/homework2_3.cpp
+++ Codes/homework2_3.cpp
@@ -1,1 +1,54 @@
-//// Created by 阮颖 on 2019/3/18.//#include <iostream>using namespace std;struct node{    int data;    node * next;    node(int d = 0, node * n = NULL){ data = d; next = n; }};class LinkNode{public:    node *head;    LinkNode(node *h){        head = h;    }    void inversion(){        node * p, * q, * r;        p = head->next->next;        q = p->next;        r = head->next;        head->next->next = NULL;        while (p->next != NULL){            p->next = r;            r = p;            p = q;            q = q->next;        }        p->next = r;        head->next = p;    }};// 测试用例int main(){    node * p = new node();    node * r = p;    for (int i = 1; i < 4; ++i) {        p->next = new node(i);        p = p->next;    }    LinkNode linkNode1(r);    linkNode1.inversion();    r = linkNode1.head->next;    while (r != NULL){        cout << r->data << endl;        r = r->next;    }}
+//
+// Created by 阮颖 on 2019/3/18.
+//
+#include <iostream>
+using namespace std;
+
+
+struct node{
+    int data;
+    node * next;
+
+    node(int d = 0, node * n = NULL){ data = d; next = n; }
+};
+
+class LinkNode{
+public:
+    node *head;
+
+    LinkNode(node *h){
+        head = h;
+    }
+
+    void inversion(){
+        node * p, * q, * r;
+        p = head->next->next;
+        q = p->next;
+        r = head->next;
+        head->next->next = NULL;
+        while (p->next != NULL){
+            p->next = r;
+            r = p;
+            p = q;
+            q = q->next;
+        }
+        p->next = r;
+        head->next = p;
+    }
+};
+// 测试用例
+int main(){
+    node * p = new node();
+    node * r = p;
+    for (int i = 1; i < 4; ++i) {
+        p->next = new node(i);
+        p = p->next;
+    }
+    LinkNode linkNode1(r);
+    linkNode1.inversion();
+    r = linkNode1.head->next;
+    while (r != NULL){
+        cout << r->data << endl;
+        r = r->next;
+    }
+}
